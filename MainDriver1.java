a8ee7e69a04ed563b0d35cb84a05d7bac2ccd58f
darshankumarjavadeveloper <113496409+darshankumarjavadeveloper@users.noreply.github.com>
2022-09-14 12:20:19 +0530

    Add files via upload

---
+++ MainDriver1.java
@@ -0,0 +1,14 @@
+
+public class MainDriver1 {
+
+	public static void main(String[] args) {
+		Employee emp=new Employee(4,"gf","flsadkfn",24314353344134l);
+		emp.displayEmployeeDetails();
+		emp.setProject(new Project(2, "darshan"));
+		emp.getProject().displayProjectDetails();
+		
+
+	}
+
+}
+                                      
